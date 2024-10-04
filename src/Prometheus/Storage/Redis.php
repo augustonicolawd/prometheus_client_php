fbff6cf2a397f9d2596c276e9661e00564abe9e3
Augusto Nicola <augusto.nicola@willdom.com>
2024-10-04 11:21:48 -0300

    Comment Redis instantiation on Storage\Redis

--- src/Prometheus/Storage/Redis.php
+++ src/Prometheus/Storage/Redis.php
@@ -58,7 +58,7 @@ class Redis implements Adapter
     public function __construct(array $options = [])
     {
         $this->options = array_merge(self::$defaultOptions, $options);
-        $this->redis = new \Redis();
+        //$this->redis = new \Redis();
     }
 
     /**
